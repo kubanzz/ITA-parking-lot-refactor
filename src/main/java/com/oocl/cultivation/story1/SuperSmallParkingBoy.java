a3f66682b1c0a18945b5af91f062974a5a042c74
benjamin <529709737@qq.com>
2020-07-25 13:39:47 +0800

    [story5][Benjamin] test: pass should_return_true_when_parkingCar_and_find_in_parkingLot2_given_parkingLot1WithSpace5_and_parkingLot2WithSpace10ParkedCar1_and_superSmallParkingBoy_and_car

--- src/main/java/com/oocl/cultivation/story1/SuperSmallParkingBoy.java
+++ src/main/java/com/oocl/cultivation/story1/SuperSmallParkingBoy.java
@@ -8,4 +8,34 @@ public class SuperSmallParkingBoy extends PackingBoy {
     public SuperSmallParkingBoy(List<PackingLot> packingLotList) {
         super(packingLotList);
     }
+
+    @Override
+    public String parkCar(Car car) {
+        PackingLot packingLot = findMaxSiteRateParkingLot();
+        if (packingLot != null && packingLot.getParkingSpace() > 0) {
+            packingLot.getPackingCarList().add(car);
+            return car.getCardId();
+        }
+        return super.parkCar(car);
+    }
+
+    private PackingLot findMaxSiteRateParkingLot() {
+        List<PackingLot> packingLotList = getPackingLotList();
+        PackingLot maxSiteRateParkingLot = null;
+        double maxSiteRate = 0;
+
+        for (PackingLot packingLot : packingLotList) {
+            if (maxSiteRateParkingLot == null || maxSiteRate < calculateSiteRate(packingLot)) {
+                maxSiteRateParkingLot = packingLot;
+                maxSiteRate = calculateSiteRate(packingLot);
+            }
+        }
+
+        return maxSiteRateParkingLot;
+    }
+
+    private double calculateSiteRate(PackingLot parkingLot) {
+        return parkingLot.getParkingSpace() / (parkingLot.getParkingSpace() + parkingLot.getPackingCarList().size());
+    }
+
 }
