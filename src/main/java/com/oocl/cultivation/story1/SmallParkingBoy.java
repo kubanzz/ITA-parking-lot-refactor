91ae8bca0f198ed49eb1ccd4add508936501078b
benjamin <529709737@qq.com>
2020-07-25 11:51:32 +0800

    [story4][Benjamin] feature: pass should_return_true_when_parkingCar_and_find_in_parkingLot2_given_parkingLot1WithSpace5_and_parkingLot2WithSpace10_and_parkingBoy_and_car

--- src/main/java/com/oocl/cultivation/story1/SmallParkingBoy.java
+++ src/main/java/com/oocl/cultivation/story1/SmallParkingBoy.java
@@ -7,4 +7,28 @@ public class SmallParkingBoy extends PackingBoy{
     public SmallParkingBoy(List<PackingLot> packingLotList) {
         super(packingLotList);
     }
+
+    @Override
+    public String parkCar(Car car) {
+        PackingLot packingLot = findSurplusMaxSpaceParkingLot();
+        packingLot.getPackingCarList().add(car);
+        return car.getCardId();
+    }
+
+    private PackingLot findSurplusMaxSpaceParkingLot() {
+        List<PackingLot> packingLotList = getPackingLotList();
+        PackingLot surplusMaxSpacParkingLot = null;
+        for (PackingLot packingLot : packingLotList) {
+            if (surplusMaxSpacParkingLot == null || surplusMaxSpacParkingLot.getParkingSpace() < packingLot.getParkingSpace()) {
+                surplusMaxSpacParkingLot = packingLot;
+            }
+        }
+
+        return surplusMaxSpacParkingLot;
+    }
+
+    @Override
+    public List<String> parkCar(List<Car> carList) {
+        return super.parkCar(carList);
+    }
 }
